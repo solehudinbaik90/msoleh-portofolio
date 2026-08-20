8ef1bb87440024d808b036063f061340457060f0
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-20 22:52:44 +0700

    Fix import path for main.css

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import "aos/dist/aos.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "react-toastify/dist/ReactToastify.css";
-import "./styles/main.css";
+import "./main.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
