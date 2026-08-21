30fc446f084530467de99d263c20992d1758f9fa
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-21 23:22:58 +0700

    Change link href to use javascript:void(0)

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -37,15 +37,15 @@ export default function News() {
                   </div>
                   <h3 className="title">{item.title}</h3>
                   <div className="msoleh_tm_read_more">
-                    <a
-                      href="#"
+                    <a 
+                      href="javascript:void(0)"
                       onClick={(e) => {
                       e.preventDefault();
                       setSelected(item);
-                      }}
-                     >
-                        <span>Buka</span>
-                    </a>
+                     }}
+                   >
+                    <span>Buka</span>
+                   </a>
                   </div>
                 </div>
               </div>
