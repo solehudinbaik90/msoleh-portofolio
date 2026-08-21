eb147828ca43245b123ccde37dfcee90f9ba3923
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-21 21:12:24 +0700

    Refactor portfolio filtering and active tab index
    Refactor portfolio filtering logic and enhance tab functionality.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -14,13 +14,17 @@ export default function Portfolio() {
   const [video, setVideo] = useState({ isOpen: false, channel: "youtube", videoId: "" });
   const [selectedDetail, setSelectedDetail] = useState(null);
 
-  const filtered = useMemo(
-    () =>
-      activeTab === "All"
-        ? portfolioItems
-        : portfolioItems.filter((i) => i.category === activeTab),
-    [activeTab]
-  );
+  const activeIndex = useMemo(() => {
+    const idx = portfolioCategories.indexOf(activeTab);
+    return idx !== -1 ? idx : 0;
+  }, [activeTab]);
+
+
+  const filteredItems = useMemo(() => {
+    return activeTab === "All"
+      ? portfolioItems
+      : portfolioItems.filter((item) => item.category === activeTab);
+  }, [activeTab]);
 
   const handleItemClick = (item) => {
     if (item.type === "vimeo" || item.type === "youtube") {
@@ -28,7 +32,6 @@ export default function Portfolio() {
     } else if (item.type === "details") {
       setSelectedDetail(item);
     }
-
   };
 
   return (
@@ -38,90 +41,95 @@ export default function Portfolio() {
 
         <div className="portfolio_filter">
           <div className="react-tabs" data-rttabs="true">
+            {/* Tab List */}
             <ul className="react-tabs__tab-list" role="tablist">
-                {portfolioCategories.map((cat, index) => {
+              {portfolioCategories.map((cat, index) => {
                 const isActive = activeTab === cat;
                 return (
                   <li
-                  key={cat}
-                  role="tab"
-                  id={`tab:r1:${index}`}
-                  aria-selected={isActive}
-                  aria-disabled="false"
-                  aria-controls={`panel:r1:${index}`}
-                  data-rttab="true"
-                  tabIndex={isActive ? 0 : -1}
-                  className={isActive ? "react-tabs__tab react-tabs__tab--selected" : "react-tabs__tab"}
-                  onClick={() => setActiveTab(cat)}
+                    key={cat}
+                    role="tab"
+                    id={`tab:r1:${index}`}
+                    aria-selected={isActive}
+                    aria-disabled="false"
+                    aria-controls={`panel:r1:${index}`}
+                    data-rttab="true"
+                    tabIndex={isActive ? 0 : -1}
+                    className={`react-tabs__tab ${isActive ? "react-tabs__tab--selected" : ""}`}
+                    onClick={() => setActiveTab(cat)}
                   >
                     {cat}
-                </li>
+                  </li>
                 );
-                })}
+              })}
             </ul>
-            <div className="list_wrapper">
-                <div className="react-tabs__tab-panel react-tabs__tab-panel--selected"
-                role="tabpanel"
-                id={`panel:r1:${portfolioCategories.indexOf(activeTab)}`}
-                aria-labelledby={`tab:r1:${portfolioCategories.indexOf(activeTab)}`}
-                >
-              <Gallery>
-                <ul className="portfolio_list">
-                  {filtered.map((item, idx) => (
-                    <li
-                      key={item.id}
-                      data-aos="fade-right"
-                      data-aos-duration="1200"
-                      data-aos-delay={idx * 100}
-                    >
-                      <div className="inner">
-                        <div className="entry msoleh_tm_portfolio_animation_wrap">
-                          {item.type === "photo" ? (
-                            <Item
-                              original={`/assets/img/portfolio/${item.image}`}
-                              thumbnail={`/assets/img/portfolio/${item.image}`}
-                              width="500"
-                              height="550"
-                            >
-                              {({ ref, open }) => (
-                                <img
-                                  ref={ref}
-                                  onClick={open}
-                                  src={`/assets/img/portfolio/${item.image}`}
-                                  alt={item.category}
-                                  data-tooltip-id={`tip-${item.id}`}
-                                  role="button"
-                                  style={{ cursor: "pointer" }}
-                                />
-                              )}
-                            </Item>
-                          ) : (
-                            <img
-                              src={`/assets/img/portfolio/${item.image}`}
-                              alt={item.category}
-                              data-tooltip-id={`tip-${item.id}`}
-                              role="button"
-                              style={{ cursor: "pointer" }}
-                              onClick={() => handleItemClick(item)}
-                            />
-                          )}
 
-                          <Tooltip
-                            id={`tip-${item.id}`}
-                            place="top"
-                            className="tooltip-wrapper"
-                          >
-                            <h5>{item.name}</h5>
-                            <span>{item.tag}</span>
-                          </Tooltip>
+            {/* Tab Panel List Wrapper */}
+            <div className="list_wrapper">
+              <div
+                className="react-tabs__tab-panel react-tabs__tab-panel--selected"
+                role="tabpanel"
+                id={`panel:r1:${activeIndex}`}
+                aria-labelledby={`tab:r1:${activeIndex}`}
+              >
+
+                <Gallery>
+                  <ul className="portfolio_list">
+                    {filteredItems.map((item, idx) => (
+                      <li
+                        key={item.id}
+                        data-aos="fade-right"
+                        data-aos-duration="1200"
+                        data-aos-delay={idx * 100}
+                      >
+                        <div className="inner">
+                          <div className="entry msoleh_tm_portfolio_animation_wrap">
+                            {item.type === "photo" ? (
+                              <Item
+                                original={`/assets/img/portfolio/${item.image}`}
+                                thumbnail={`/assets/img/portfolio/${item.image}`}
+                                width="500"
+                                height="550"
+                              >
+                                {({ ref, open }) => (
+                                  <img
+                                    ref={ref}
+                                    onClick={open}
+                                    src={`/assets/img/portfolio/${item.image}`}
+                                    alt={item.name || item.category}
+                                    data-tooltip-id={`tip-${item.id}`}
+                                    role="button"
+                                    style={{ cursor: "pointer" }}
+                                  />
+                                )}
+                              </Item>
+                            ) : (
+                              <img
+                                src={`/assets/img/portfolio/${item.image}`}
+                                alt={item.name || item.category}
+                                data-tooltip-id={`tip-${item.id}`}
+                                role="button"
+                                style={{ cursor: "pointer" }}
+                                onClick={() => handleItemClick(item)}
+                              />
+                            )}
+
+
+                            <Tooltip
+                              id={`tip-${item.id}`}
+                              place="top"
+                              className="tooltip-wrapper"
+                            >
+                              <h5>{item.name}</h5>
+                              <span>{item.tag}</span>
+                            </Tooltip>
+                          </div>
                         </div>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </Gallery>
+                      </li>
+                    ))}
+                  </ul>
+                </Gallery>
               </div>
-             </div>
             </div>
           </div>
         </div>
